9dba8404b2f21f4e42ce66cde59b5c8a9354a8ec
So Muzaffari <so.muzaff@gmail.com>
2026-01-06 07:36:07 +0330

    Update UnifiedStorageAccess.kt

--- app/src/main/kotlin/com/watermelon/player/storage/UnifiedStorageAccess.kt
+++ app/src/main/kotlin/com/watermelon/player/storage/UnifiedStorageAccess.kt
@@ -5,29 +5,40 @@ import android.content.Context
 import android.provider.MediaStore
 import com.watermelon.player.database.VideoEntity
 
+/**
+ * [DATA LAYER: UnifiedStorageAccess]
+ * * ENHANCEMENT: Scoped Storage Compatibility.
+ * This handles the complexity of Android 11+ MediaStore queries while 
+ * integrating our "Folder Visibility" database filters.
+ */
 class UnifiedStorageAccess(private val context: Context) {
 
     /**
-     * Scans the device for video files and returns a list of VideoEntities.
-     * This logic bypasses folders that the user has toggled "OFF".
+     * [FUNCTION: fetchLocalVideos]
+     * * FIX: Avoids the "Slow Vault" problem. 
+     * Instead of encrypting files to hide them (which is slow), we filter 
+     * them out during the scan phase using our Exclusion List.
+     * * @param excludedFolders A list of folder paths the user has toggled "OFF".
      */
     fun fetchLocalVideos(excludedFolders: List<String>): List<VideoEntity> {
         val videoList = mutableListOf<VideoEntity>()
         
+        // Define which file data we need from the phone.
         val projection = arrayOf(
             MediaStore.Video.Media._ID,
             MediaStore.Video.Media.DISPLAY_NAME,
             MediaStore.Video.Media.DURATION,
             MediaStore.Video.Media.SIZE,
-            MediaStore.Video.Media.DATA // Path to check against exclusion
+            MediaStore.Video.Media.DATA // The absolute disk path.
         )
 
+        // Query the Android MediaStore Database.
         val query = context.contentResolver.query(
             MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
             projection,
             null,
             null,
-            "${MediaStore.Video.Media.DATE_ADDED} DESC"
+            "${MediaStore.Video.Media.DATE_ADDED} DESC" // Show newest first.
         )
 
         query?.use { cursor ->
@@ -39,14 +50,19 @@ class UnifiedStorageAccess(private val context: Context) {
             while (cursor.moveToNext()) {
                 val path = cursor.getString(pathColumn)
                 
-                // --- INTEGRATED HIDE/SHOW LOGIC ---
-                // If the folder path is in our exclusion list, we skip it entirely
+                // --- INTEGRATED HIDE/SHOW FILTER ---
+                // We check if the current file's path contains any string 
+                // from our "Invisible" folders list.
                 val isExcluded = excludedFolders.any { path.contains(it) }
+                
+                // If it's in a hidden folder, we skip adding it to the UI.
                 if (isExcluded) continue
 
                 val id = cursor.getLong(idColumn)
                 val name = cursor.getString(nameColumn)
                 val duration = cursor.getLong(durationColumn)
+                
+                // Construct the "Playable URI" for Media3.
                 val contentUri = ContentUris.withAppendedId(
                     MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id
                 )
@@ -56,11 +72,11 @@ class UnifiedStorageAccess(private val context: Context) {
                     title = name,
                     uri = contentUri.toString(),
                     duration = duration,
-                    folderPath = path.substringBeforeLast("/")
+                    folderPath = path.substringBeforeLast("/") // Get the parent folder.
                 ))
             }
         }
-        return videoList
+        return videoList // Returns only "Visible" media to the Home Screen.
     }
 }
 

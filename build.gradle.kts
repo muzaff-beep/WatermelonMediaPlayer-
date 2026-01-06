cc63db2c8bb2a68c33adc5486e7cbef45c3e44f2
So Muzaffari <so.muzaff@gmail.com>
2026-01-06 07:41:21 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,7 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    }
+// [ENGINEERING BLOCK: TOP-LEVEL BUILD]
+// Defines the versions of the tools used to build Watermelon Player.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}

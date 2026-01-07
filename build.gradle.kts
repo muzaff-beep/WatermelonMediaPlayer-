60e74f66506ce604b48afd3f93adb9586f8c2a66
So Muzaffari <so.muzaff@gmail.com>
2026-01-07 04:40:49 +0330

    Update build.gradle.kts
    Signed-off-by: So Muzaffari  <so.muzaff@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-    id("com.android.application") version "8.7.0" apply false // Latest stable AGP
-}
+    id("com.android.application") version "8.6.0" apply false
 
 allprojects {
     repositories {
